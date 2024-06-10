0af29dfe3ba51cf55a5554849ad275c47dbdecd3
LucasLvy <lucaslevy10@gmail.com>
2024-06-10 14:24:33 +0200

    feat(grammar): add cairo grammar + queries

---
+++ Package.swift
@@ -0,0 +1,47 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "TreeSitterCairo",
+    products: [
+        .library(name: "TreeSitterCairo", targets: ["TreeSitterCairo"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "TreeSitterCairo",
+                path: ".",
+                exclude: [
+                    "Cargo.toml",
+                    "Makefile",
+                    "binding.gyp",
+                    "bindings/c",
+                    "bindings/go",
+                    "bindings/node",
+                    "bindings/python",
+                    "bindings/rust",
+                    "prebuilds",
+                    "grammar.js",
+                    "package.json",
+                    "package-lock.json",
+                    "pyproject.toml",
+                    "setup.py",
+                    "test",
+                    "examples",
+                    ".editorconfig",
+                    ".github",
+                    ".gitignore",
+                    ".gitattributes",
+                    ".gitmodules",
+                ],
+                sources: [
+                    "src/parser.c",
+                    // NOTE: if your language has an external scanner, add it here.
+                ],
+                resources: [
+                    .copy("queries")
+                ],
+                publicHeadersPath: "bindings/swift",
+                cSettings: [.headerSearchPath("src")])
+    ],
+    cLanguageStandard: .c11
+)
